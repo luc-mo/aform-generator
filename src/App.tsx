0f090944742f26fa702b9bc829b4cd0898e957ca
SnowDrive <luc.moralesr@gmail.com>
2023-06-27 14:18:08 -0400

    feat: add form count state to show forms one by one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { FC, ReactElement } from 'react'
+import { AitorForm } from 'components/AitorForm'
+import { FiuterForm } from 'components/FiuterForm'
+import { Form1 } from 'components/Form1'
+import { Form2 } from 'components/Form2'
+import { FC, FormEvent, ReactElement, useCallback, useState } from 'react'
 
 export const App: FC = (): ReactElement => {
+  const [formCount, setFormCount] = useState<number>(0)
+
+  const handleFormCount = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setFormCount(prev => prev + 1)
+  }, [])
+
+  const handleLast = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+  }
+
   return (
     <>
-      
+      <FiuterForm onSubmit={handleFormCount}/>
+      {formCount > 0 && <Form1 onSubmit={handleFormCount} />}
+      {formCount > 1 && <AitorForm onSubmit={handleFormCount} />}
+      {formCount > 2 && <Form2 onSubmit={handleLast}/>}
     </>
   )
 }
