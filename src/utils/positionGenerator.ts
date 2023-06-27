15ceb663409c3ff9bbc1bdd62aae27a23808637d
SnowDrive <luc.moralesr@gmail.com>
2023-06-27 13:06:27 -0400

    feat: modify position generator limits

--- src/utils/positionGenerator.ts
+++ src/utils/positionGenerator.ts
@@ -11,13 +11,14 @@ interface Return {
 }
 
 export const positionGenerator = ({ position, speed, size, axis }: PositionGenerator): Return => {
+  const { clientWidth, clientHeight } = document.body
   const next = position + speed
   if(axis === 'x') {
-    if(next + size > window.innerWidth) return { position: window.innerWidth - size, speed: speed * -1 }
+    if(next + size > clientWidth) return { position: clientWidth - size, speed: speed * -1 }
     else if(next < 0) return { position: 0, speed: speed * -1 }
     return { position: next, speed }
   } else {
-    if(next + size > window.innerHeight) return { position: window.innerHeight - size, speed: speed * -1 }
+    if(next + size > clientHeight) return { position: clientHeight - size, speed: speed * -1 }
     else if(next < 0) return { position: 0, speed: speed * -1 }
     return { position: next, speed }
   }
